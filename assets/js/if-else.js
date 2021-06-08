f8cfd5075b6017c55ec002053328a9e11395e4d2
CarlosMRDronvicion <carlosmendezrodriguez1997@gmail.com>
2021-06-08 12:58:47 +0200

    aprendiendo a hacer alternativas a IfElse

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -29,3 +29,23 @@ if ( dia === 0){  //aqui ponemos tres = porque estamos haciendo una condicion de
 }else{
     console.log("No es lunes, martes o domingo...");
 }
+
+//Sin usar If else o switch, solo con objetos 
+dia = 4; //0: domingo, 1:lunes...
+
+const diaLetras = {
+    0: "domingo",
+    1: "lunes",
+    2: "martes", 
+    3: "miercoles",
+    4: "jueves",
+    5: "viernes",
+    6: "sabado",
+}
+
+const diaLetras2 = [ "domingo", "lunes", "martes",  "miercoles", "jueves", "viernes", "sabado"]; //esto es una alternativa al ejercicio de arriba
+
+
+console.log( diaLetras2[dia] || "Dia no es valido");
+
+
