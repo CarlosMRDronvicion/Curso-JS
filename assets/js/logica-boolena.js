68bca6fe0e30bc67c428fc1ac481986f573e13d1
CarlosMRDronvicion <carlosmendezrodriguez1997@gmail.com>
2021-06-08 16:02:39 +0200

    finalizando clase logica booleanos

--- assets/js/logica-boolena.js
+++ assets/js/logica-boolena.js
@@ -41,3 +41,25 @@ console.log( true || false);
 console.log( regresaTrue() || regresaFalse() );
 
 console.log("4 Condiciones ", true || true || true || false); //true
+
+
+console.warn("Asignaciones");
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && "HolaMundo";
+const a2 = "Hola" && "Mundo"; //si hay algun valor false o soyFalso imprime falso
+const a3 = soyFalso || " ya no soy falso";
+const a4 = soyFalso || soyNull || soyUndefined || " ya no soy falso de nuevo";
+const a5 = soyFalso || regresaTrue () || soyUndefined || " ya no soy falso de nuevo" || true;
+
+console.log({a1, a2, a3, a4, a5});
+
+
+if ( regresaFalse() && regresaTrue() (true || true || true || false) ){
+    console.log("hacer algo");
+}else{
+    console.log("hacer otra cosa");
+}
